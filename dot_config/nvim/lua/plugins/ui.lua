1a171a5a1741271765d82a52637baafdbf4a558d
Omar Ramadan <oramadan@chaizer.com>
2025-12-17 15:42:28 +0300

    removed mini files manager

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -29,17 +29,6 @@ return {
             require("mini.ai").setup({ n_lines = 500 })
             -- Add/delete/replace surroundings (brackets, quotes, etc.)
             require("mini.surround").setup()
-            -- File manager
-            require("mini.files").setup({
-                mappings = {
-                    close = "<Esc>",
-                },
-            })
-
-            vim.keymap.set("n", "<leader>e", function()
-                require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-            end, { desc = "Open Mini Files (Current File)" })
-
             -- Simple and easy statusline.
             local statusline = require("mini.statusline")
             statusline.setup({ use_icons = vim.g.have_nerd_font })
