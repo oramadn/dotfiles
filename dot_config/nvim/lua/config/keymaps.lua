3ce9a0d62cec018058e4bbd90e4dfc9faa2d995f
Omar Ramadan <oramadan@chaizer.com>
2025-12-19 17:25:45 +0300

    added yank link keymap, ruby formatter and enhanced telescope search keybinds

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -14,3 +14,12 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Yank file path + line number
+vim.keymap.set("n", "<leader>yl", function()
+    local path = vim.fn.expand("%:p:.")
+    local line = vim.fn.line(".")
+    local text = path .. ":" .. line
+    vim.fn.setreg("+", text)
+    print("Yanked: " .. text)
+end, { desc = "[Y]ank file:[L]ine" })
--- dot_config/nvim/lua/plugins/format.lua
+++ dot_config/nvim/lua/plugins/format.lua
@@ -1,34 +1,35 @@
 return {
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
+    { -- Autoformat
+        "stevearc/conform.nvim",
+        event = { "BufWritePre" },
+        cmd = { "ConformInfo" },
+        keys = {
+            {
+                "<leader>f",
+                function()
+                    require("conform").format({ async = true, lsp_format = "fallback" })
+                end,
+                mode = "",
+                desc = "[F]ormat buffer",
+            },
+        },
+        opts = {
+            notify_on_error = false,
+            format_on_save = function(bufnr)
+                if vim.b.conform_disable_format_on_save then
+                    return nil
+                end
+
+                local disable_filetypes = { c = true, cpp = true, ruby = true, }
+                if disable_filetypes[vim.bo[bufnr].filetype] then
+                    return nil
+                else
+                    return {
+                        timeout_ms = 500,
+                        lsp_format = "fallback",
+                    }
+                end
+            end,
+        },
     },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-      },
-    },
-  },
 }
