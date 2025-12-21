3f60e49bd63518c621bd1f44d8271000b76f7aa2
Omar Ramadan <omar.ramadanpersonal@gmail.com>
2025-12-21 12:31:40 +0300

    Added yank line number keybind. Removed commands file

--- dot_config/nvim/lua/config/commands.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_create_user_command("FormatToggle", function()
-    vim.b.conform_disable_format_on_save = not vim.b.conform_disable_format_on_save
-
-    print(vim.b.conform_disable_format_on_save and "Format on save: disabled" or "Format on save: enabled")
-end, { desc = "Toggle format on save for this buffer" })
